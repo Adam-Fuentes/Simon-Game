603046f459190432c77b74ee0ac9c98d68e9dcea
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-10-28 11:13:42 +0100

    add started var and refactor

--- js/game.js
+++ js/game.js
@@ -3,36 +3,44 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePatter = [];
 var userClickedPattern = [];
-//add lvel
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false;
+
+//2. Create a new variable called level and start at level 0.
 var level = 0;
 
-$(".btn").click(function() {
+//1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
+$(document).on('keypress',function() {
+    if(!started){
+        //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
 
+$(".btn").click(function() {
     var userChosenColour = $(this).attr("id");  
     userClickedPattern.push(userChosenColour);
 
     playSound(userChosenColour);
+    animatePress(userChosenColour);
   
-});
-// add press any key
-$(document).on('keypress',function() {
-    nextSequence();
 });
 
 function nextSequence() {
-    //change title
-    $("h1").text("Level "+level);
+    //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+    level++;
+
+    //5. Inside nextSequence(), update the h1 with this change in the value of level.
+    $("#level-title").text("Level " + level);
 
     var randomNumber = Math.floor((Math.random() * 4));
     var randomChosenColour = buttonColours[randomNumber];
     gamePatter.push(randomChosenColour);
 
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);    
-
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);  
     playSound(randomChosenColour);
-    // increment
-    level++;
-
 };
 
 function playSound(name) {
