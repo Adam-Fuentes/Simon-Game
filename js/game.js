7ced5d9963b301fe8a265fd2c1822a79d604c785
Adam Fuentes <66355715+Adam-Fuentes@users.noreply.github.com>
2020-10-28 10:48:35 +0100

    add new function, playSound

--- js/game.js
+++ js/game.js
@@ -1,33 +1,36 @@
 
-var gamePatter = [];
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
+var gamePatter = [];
 var userClickedPattern = [];
 
-//1. Use jQuery to detect when any of the buttons are clicked and trigger a handler function.
 $(".btn").click(function() {
 
-    //2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked.
-    var userChosenColour = $(this).attr("id");
-  
-    //4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+    var userChosenColour = $(this).attr("id");  
     userClickedPattern.push(userChosenColour);
+
+    //1. In the same way we played sound in nextSequence() , when a user clicks on a button, the corresponding sound should be played.
+    playSound(userChosenColour);
   
   });
 
 function nextSequence() {
 
-    //generamos aleatorio y lo ponemos el la ultima posicion del array
     var randomNumber = Math.floor((Math.random() * 4));
     var randomChosenColour = buttonColours[randomNumber];
     gamePatter.push(randomChosenColour);
 
-    //selecionamos el mismo id de html que del color random y le damos una animacion
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);    
 
-    //le ponemos sonido al color seleccionado
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-    audio.play();
+    //4. Refactor the code in playSound() so that it will work for both playing sound in nextSequence() and when the user clicks a button.
+    playSound(randomChosenColour);
 
 };
+
+//2. Create a new function called playSound() that takes a single input parameter called name.
+function playSound(name) {
+
+    //3. Take the code we used to play sound in the nextSequence() function and add it to playSound().
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+  }
